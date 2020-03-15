9aef284620953416430754ac7d56d09a7d1c0f0d
qinghuan <hashlock@163.com>
2020-03-15 11:55:52 +0800

    update ink2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,22 @@
+#![feature(proc_macro_hygiene)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use ink_core::{
-    storage,
-    memory::vec::Vec,
-};
-use ink_lang::contract;
+use ink_lang as ink;
 
-pub type Text = Vec<u8>;
+#[ink::contract(version = "0.1.0", env = DefaultXrmlTypes)]
+mod simple_pns {
+    use ink_core::{
+        env::DefaultXrmlTypes,
+        storage,
+    };
+    use scale::{
+        Encode
+    };
+    use ink_prelude::vec::Vec;
 
-contract! {
-    #![env = ink_core::env::DefaultSrmlTypes]
+    pub type Text = Vec<u8>;
 
-    event Register {
-        name: Hash,
-        from: AccountId,
-    }
-
-    event RegisterAbi {
-        from: AccountId,
-        name: Hash,
-        code_hash: Hash,
-        abi: Text
-    }
-
-    event SetAddress {
-        name: Hash,
-        from: AccountId,
-        old_address: Option<AccountId>,
-        new_address: AccountId,
-    }
-
-    event Transfer {
-        name: Hash,
-        from: AccountId,
-        old_owner: Option<AccountId>,
-        new_owner: AccountId,
-    } 
-
+    #[ink(storage)]
     struct SimplePns {
         /// A hashmap to store all name to addresses mapping
         name_to_address: storage::HashMap<Hash, AccountId>,
@@ -48,33 +28,73 @@ contract! {
         default_address: storage::Value<AccountId>,
     }
 
-    impl Deploy for SimplePns {
-        /// Initializes contract with default address.
-        fn deploy(&mut self) {
-            self.default_address.set(AccountId::from([0x0; 32]));
-        }
+    /// Emitted whenever a new name is being registered.
+    #[ink(event)]
+    struct Register {
+        #[ink(topic)]
+        name: Hash,
+        #[ink(topic)]
+        from: AccountId,
     }
 
+    /// Emitted whenever a new name is being registered.
+    #[ink(event)]
+    struct RegisterAbi {
+        #[ink(topic)]
+        name: Hash,
+        #[ink(topic)]
+        from: AccountId,
+        #[ink(topic)]
+        code_hash: Hash,
+        abi: Text
+    }
+
+    #[ink(event)]
+    struct SetAddress {
+        name: Hash,
+        from: AccountId,
+        old_address: Option<AccountId>,
+        new_address: AccountId,
+    }
+
+    #[ink(event)]
+    struct Transfer {
+        name: Hash,
+        #[ink(topic)]
+        from: AccountId,
+        #[ink(topic)]
+        old_owner: Option<AccountId>,
+        #[ink(topic)]
+        new_owner: AccountId,
+    } 
+
     impl SimplePns {
+        /// Creates a new domain name service contract.
+        #[ink(constructor)]
+        fn new(&mut self) {
+            self.default_address.set(AccountId::from([0x0; 32]));
+        }
+
         /// Register specific name with caller as owner
-        pub(external) fn register(&mut self, name: Hash, address: AccountId) -> bool {
-            let caller = env.caller();
+        #[ink(message)]
+        fn register(&mut self, name: Hash, address: AccountId) -> bool {
+            let caller = self.env().caller();
             if self.is_name_exist_impl(name) {
                 return false
             }
             // env.println(&format!("register name: {:?}, owner: {:?}", name, caller));
             self.name_to_owner.insert(name, caller);
             self.name_to_address.insert(name, address);
-            env.emit(Register {
+            self.env().emit_event(Register {
                 name: name,
                 from: caller,
             });
             true
         }
 
-        /// Register abi with name and code_hash
-        pub(external) fn register_abi(&mut self, name: Hash, code_hash: Hash, abi: Text) -> bool {
-            let caller = env.caller();
+        #[ink(message)]
+        fn register_abi(&mut self, name: Hash, code_hash: Hash, abi: Text) -> bool {
+            let caller = self.env().caller();
             if self.is_name_exist_impl(name) {
                 return false
             }
@@ -91,7 +111,7 @@ contract! {
                     a.push(code_hash);
                 }
             }
-            env.emit(RegisterAbi {
+            self.env().emit_event(RegisterAbi {
                 from: caller,
                 name: name,
                 code_hash: code_hash,
@@ -100,31 +120,35 @@ contract! {
             true
         }
 
-        /// Query abi by name
-        pub(external) fn get_abi_by_name(&self, name: Hash) -> Text {
+         /// Query abi by name
+        #[ink(message)]
+        fn get_abi_by_name(&self, name: Hash) -> Text {
             self.name_to_abi.get(&name).unwrap_or(&Vec::new()).to_vec()
         }
 
         /// Query abi by code_hash
-        pub(external) fn get_abi_by_code_hash(&self, code_hash: Hash) -> Text {
+        #[ink(message)]
+        fn get_abi_by_code_hash(&self, code_hash: Hash) -> Text {
             self.code_hash_to_abi.get(&code_hash).unwrap_or(&Vec::new()).to_vec()
         }
 
         /// Query code_hash list by account
-        pub(external) fn get_code_hash_list_by_account(&self, account: AccountId) -> Vec<Hash> {
+        #[ink(message)]
+        fn get_code_hash_list_by_account(&self, account: AccountId) -> Vec<Hash> {
             self.account_to_code_hash_list.get(&account).unwrap_or(&Vec::new()).to_vec()
         }
 
         /// Set address for specific name
-        pub(external) fn set_address(&mut self, name: Hash, address: AccountId) -> bool {
-            let caller: AccountId = env.caller();
+        #[ink(message)]
+        fn set_address(&mut self, name: Hash, address: AccountId) -> bool {
+            let caller: AccountId = self.env().caller();
             let owner: AccountId = self.get_owner_or_none(name);
             // env.println(&format!("set_address caller: {:?}, owner: {:?}", caller, owner));
             if caller != owner {
                 return false
             }
             let old_address = self.name_to_address.insert(name, address);
-            env.emit(SetAddress {
+            self.env().emit_event(SetAddress {
                 name: name,
                 from: caller,
                 old_address: old_address,
@@ -134,15 +158,16 @@ contract! {
         }
 
         /// Transfer owner to another address
-        pub(external) fn transfer(&mut self, name: Hash, to: AccountId) -> bool {
-            let caller: AccountId = env.caller();
+        #[ink(message)]
+        fn transfer(&mut self, name: Hash, to: AccountId) -> bool {
+            let caller: AccountId = self.env().caller();
             let owner: AccountId = self.get_owner_or_none(name);
             // env.println(&format!("transfer caller: {:?}, owner: {:?}", caller, owner));
             if caller != owner {
                 return false
             }
             let old_owner = self.name_to_owner.insert(name, to);
-            env.emit(Transfer {
+            self.env().emit_event(Transfer {
                 name: name,
                 from: caller,
                 old_owner: old_owner,
@@ -152,22 +177,20 @@ contract! {
         }
 
         /// Get address for the specific name 
-        pub(external) fn get_address(&self, name: Hash) -> AccountId {
+        #[ink(message)]
+        fn get_address(&self, name: Hash) -> AccountId {
             let address: AccountId = self.get_address_or_none(name);
             // env.println(&format!("get_address name is {:?}, address is {:?}", name, address));
             address
         }
 
         /// Check whether name is exist
-        pub(external) fn is_name_exist(&self, name: Hash) -> bool {
+        #[ink(message)]
+        fn is_name_exist(&self, name: Hash) -> bool {
             self.is_name_exist_impl(name)
         }
-    }
 
-    /// Implement some private methods
-    impl SimplePns {
-        /// Returns an AccountId or default 0x00*32 if it is not set.
-        fn get_address_or_none(&self, name: Hash) -> AccountId {
+         fn get_address_or_none(&self, name: Hash) -> AccountId {
             let address = self.name_to_address.get(&name).unwrap_or(&self.default_address);
             *address
         }
@@ -188,70 +211,70 @@ contract! {
         }
     }
 
-}
+    #[cfg(all(test, feature = "test-env"))]
+    mod tests {
+        use super::*;
+        use ink_core::env;
+        type Types = ink_core::env::DefaultSrmlTypes;
 
-#[cfg(all(test, feature = "test-env"))]
-mod tests {
-    use super::*;
-    use ink_core::env;
-    type Types = ink_core::env::DefaultSrmlTypes;
+        #[test]
+        fn register_works() {
+            let alice = AccountId::from([0x1; 32]);
+            // let bob: AccountId = AccountId::from([0x2; 32]);
+            let name = Hash::from([0x99; 32]);
 
-    #[test]
-    fn register_works() {
-        let alice = AccountId::from([0x1; 32]);
-        // let bob: AccountId = AccountId::from([0x2; 32]);
-        let name = Hash::from([0x99; 32]);
+            let mut contract = SimplePns::deploy_mock();
+            env::test::set_caller::<Types>(alice);
 
-        let mut contract = SimplePns::deploy_mock();
-        env::test::set_caller::<Types>(alice);
-
-        assert_eq!(contract.register(name, alice), true);
-        assert_eq!(contract.register(name, alice), false);
-    }
+            assert_eq!(contract.register(name, alice), true);
+            assert_eq!(contract.register(name, alice), false);
+        }
     
-    #[test]
-    fn set_address_works() {
-        let alice = AccountId::from([0x1; 32]);
-        let bob: AccountId = AccountId::from([0x2; 32]);
-        let name = Hash::from([0x99; 32]);
+        #[test]
+        fn set_address_works() {
+            let alice = AccountId::from([0x1; 32]);
+            let bob: AccountId = AccountId::from([0x2; 32]);
+            let name = Hash::from([0x99; 32]);
 
-        let mut contract = SimplePns::deploy_mock();
-        env::test::set_caller::<Types>(alice);
+            let mut contract = SimplePns::deploy_mock();
+            env::test::set_caller::<Types>(alice);
 
-        assert_eq!(contract.register(name, alice), true);
+            assert_eq!(contract.register(name, alice), true);
 
-        // caller is not owner, set_address will be failed
-        env::test::set_caller::<Types>(bob);
-        assert_eq!(contract.set_address(name, bob), false);
+            // caller is not owner, set_address will be failed
+            env::test::set_caller::<Types>(bob);
+            assert_eq!(contract.set_address(name, bob), false);
 
-        // caller is owner, set_address will be successful
-        env::test::set_caller::<Types>(alice);
-        assert_eq!(contract.set_address(name, bob), true);
+            // caller is owner, set_address will be successful
+            env::test::set_caller::<Types>(alice);
+            assert_eq!(contract.set_address(name, bob), true);
 
-        assert_eq!(contract.get_address(name), bob);
-    }
+            assert_eq!(contract.get_address(name), bob);
+        }
 
-    #[test]
-    fn transfer_works() {
-        let alice = AccountId::from([0x1; 32]);
-        let bob = AccountId::from([0x2; 32]);
-        let name = Hash::from([0x99; 32]);
+        #[test]
+        fn transfer_works() {
+            let alice = AccountId::from([0x1; 32]);
+            let bob = AccountId::from([0x2; 32]);
+            let name = Hash::from([0x99; 32]);
 
-        let mut contract = SimplePns::deploy_mock();
-        env::test::set_caller::<Types>(alice);
+            let mut contract = SimplePns::deploy_mock();
+            env::test::set_caller::<Types>(alice);
 
-        assert_eq!(contract.register(name, alice), true);
+            assert_eq!(contract.register(name, alice), true);
 
-        // transfer owner
-        assert_eq!(contract.transfer(name, bob), true);
+            // transfer owner
+            assert_eq!(contract.transfer(name, bob), true);
 
-        // now owner is bob, alice set_address will be failed
-        assert_eq!(contract.set_address(name, bob), false);
+            // now owner is bob, alice set_address will be failed
+            assert_eq!(contract.set_address(name, bob), false);
 
-        env::test::set_caller::<Types>(bob);
-        // now owner is bob, set_address will be successful
-        assert_eq!(contract.set_address(name, bob), true);
+            env::test::set_caller::<Types>(bob);
+            // now owner is bob, set_address will be successful
+            assert_eq!(contract.set_address(name, bob), true);
 
-        assert_eq!(contract.get_address(name), bob);
+            assert_eq!(contract.get_address(name), bob);
+        }
     }
 }
+
